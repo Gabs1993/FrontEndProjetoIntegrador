03336ecb6b052f13230e8d8780ba766a53a0f4d6
unknown <gabrielcsantos1993@gmail.com>
2021-08-26 16:02:20 -0300

    Front-projetointegrador

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: 'menu', pathMatch: 'full'},
+
+
+  {path: 'menu', component: MenuComponent },
+  {path: 'home', component: HomeComponent},
+  {path: 'rodape', component: RodapeComponent},
+  {path: 'sobre-nos', component: SobreNosComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SobreNosComponent,
+    HomeComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
