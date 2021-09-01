e44ef3c8e35c046898009f5824e42dcfd6c82f27
Débora Cristina Santos <deboracristinasantos5@gmail.com>
2021-09-01 12:30:46 -0300

    implementacao do component rotas

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'menu', component: MenuComponent },
   {path: 'home', component: HomeComponent},
   {path: 'rodape', component: RodapeComponent},
-  {path: 'sobre-nos', component: SobreNosComponent}
+  {path: 'sobre-nos', component: SobreNosComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
 
 
 ];
